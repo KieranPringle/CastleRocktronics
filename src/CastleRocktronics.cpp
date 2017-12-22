60503e6552f1b82fef9684e3b85d4775bd4b8dbe
Kieran Pringle <kpringe@adobe.com>
2017-12-22 09:25:40 +0000

    Add switched multiple module
    commit 03e81619f7f1b76737f10c61ab8af508ec3957e5
    Author: Kieran Pringle <kpringe@adobe.com>
    Date:   Fri Dec 22 09:25:12 2017 +0000
    
        Refactor headers and remove unused blank module
    
    commit 251c2e2693fb1c91ec91286a907491df91066eb4
    Author: Kieran Pringle <kpringe@adobe.com>
    Date:   Fri Dec 22 09:01:09 2017 +0000
    
        Add panel graphic
    
    commit 06750f70f163c66d63e5619da7bbfd2e191a808b
    Author: Kieran Pringle <kpringe@adobe.com>
    Date:   Thu Dec 21 14:00:36 2017 +0000
    
        Place UI elements with iterators
    
    commit a6a2e8dcf91015afc95e0d8efce47a5053feda0f
    Author: Kieran Pringle <kpringe@adobe.com>
    Date:   Thu Dec 21 13:30:09 2017 +0000
    
        Add switching logic
    
    commit 9168927f5e4f30739649341f62610a2d5fc0da8f
    Author: Kieran Pringle <kpringe@adobe.com>
    Date:   Thu Dec 21 13:14:35 2017 +0000
    
        Basic placement of UI elements done
    
    commit 440e5c98bc59c8c01bfe4f68fa0bbae19f6928f9
    Author: Kieran Pringle <kpringe@adobe.com>
    Date:   Thu Dec 21 11:27:35 2017 +0000
    
        Create SwitchedMultiple.cpp
    
    commit 6e54648cc4f6449638287476dd4981978dd9f337
    Author: Kieran Pringle <kpringe@adobe.com>
    Date:   Wed Dec 20 14:47:19 2017 +0000
    
        Update gitignore and add CastleRocktronics.cpp/hpp

---
+++ src/CastleRocktronics.cpp
@@ -0,0 +1,21 @@
+#include "SwitchedMultiple.hpp"
+
+// The plugin-wide instance of the Plugin class
+Plugin *plugin;
+
+void init(rack::Plugin *p) {
+    plugin = p;
+    // This is the unique identifier for your plugin
+    p->slug = "CastleRocktronics";
+#ifdef VERSION
+    p->version = TOSTRING(VERSION);
+#endif
+    p->website = "";
+    p->manual = "";
+    
+    // For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
+    p->addModel(createModel<SwitchedMultipleWidget>("CastleRocktronics", "CR-V02_SwitchedMultiple", "Switched Multiple", UTILITY_TAG, MULTIPLE_TAG));
+    
+    // Any other plugin initialization may go here.
+    // As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+}
---
+++ src/SwitchedMultiple.hpp
@@ -0,0 +1,19 @@
+#include "rack.hpp"
+
+using namespace rack;
+
+extern Plugin *plugin;
+
+struct SwitchedMultipleWidget : ModuleWidget {
+    SwitchedMultipleWidget();
+
+private:
+    int moduleWidth;
+
+    void placeGUIElements();
+    void placeInputs(int startX, int startY, int spacing);
+    void placeOutputs(int startX, int startY, int spacing);
+    void placeSwitches(int startX, int startY, int spacing);
+    void placeScrews();
+
+};
